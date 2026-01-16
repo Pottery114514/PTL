95099bb24de17f8de780b4e8bc0499ca5b5374f6
Your Name <you@example.com>
2026-01-17 00:05:35 +0800

    nothing to say

--- fac_query_inv.cpp
+++ fac_query_inv.cpp
@@ -4,7 +4,12 @@ using ll=long long;
 #define REP(i,s,e) for(ll i=s;i<=e;++i)
 const int P=1e9+7;
 const int N=1e7+7;
-
+ll fac[N];
+void init(){
+    fac[0]=1;//定义如此
+    REP(i,1,N)fac[i]=fac[i-1]*i%P;
+}
+//求逆元
 ll qmi(ll a,ll b){
     ll res=1;
     while(b){
@@ -13,9 +18,16 @@ ll qmi(ll a,ll b){
     }
     return res;
 }
-
 ll inv(ll x){return qmi(x,P-2);}
 
 ll C(ll n,ll m){
-    
+    if(n<0||m<0||n<m)return 0;
+    return fac[n]*inv(fac[m])%P*inv(fac[n-m])%P;//一定要在每次乘法后立即取模
+}
+
+int main(){
+    init();
+    int n,m;cin>>n>>m;
+    cout<<C(n,m)<<'\n';
+    return 0;
 }
